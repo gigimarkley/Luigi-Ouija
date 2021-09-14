e0566d1317182709c2d255433d1f428911b11223
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-09-14 09:58:05 -0700

    can now see video and ouija board on the same screen

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
 import store from "./store";
 import App from "./App";
+import webgazer from "webgazer";
 
 ReactDOM.render(
   <Provider store={store}>
